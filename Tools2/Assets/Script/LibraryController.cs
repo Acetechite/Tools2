e4947c45cbb9faeb3e0da085080dbe012be8ece0
Acetechite <31084818+Acetechite@users.noreply.github.com>
2017-09-20 18:55:31 -0400

    Created Text classes with hub call
    There are now classes you can attach to text objects that override thier text field but are immediately connected to a hub of languages and words that are hosted in a main language class

--- Tools2/Assets/Script/LibraryController.cs
+++ Tools2/Assets/Script/LibraryController.cs
@@ -5,22 +5,72 @@ using UnityEngine.UI;
 
 [ExecuteInEditMode]
 public class LibraryController : MonoBehaviour {
-
-    public GameObject menuItem;
-    public Text textHolder;
+    
     public TextAsset library;
     public string fileText;
     public string menuText;
 
+    public string textToUse;
+    public string textTranslated;
+
+    public string selectedLang;
+    public int langIndex;
+    public string[] languages;
+    public string[] contents;
+
 	// Use this for initialization
 	void OnEnable () {
-        fileText = library.text;
-        textHolder = menuItem.GetComponent<Text>();
-        textHolder.text = fileText;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        char[] separators = { ':', ',' };
+        fileText = library.text;
+        Debug.Log(fileText);
+
+        contents = fileText.Split(separators);
+
+        //check for issues
+
+        langIndex = -1;
+        //search for lang
+        for (int i = 0; i < languages.Length; i++)
+        {
+            if (languages[i] == selectedLang)
+            {
+                langIndex = i;
+            }
+        }
+        if (langIndex == -1)
+        {
+            Debug.LogError("The selected language has not been added");
+        }
+
+        //search for word
+        for (int i = 0; i < contents.Length; i++)
+        {
+            if (contents[i] == textToUse)
+            {
+                textTranslated = contents[i + langIndex];
+            }
+        }
+    }
+
+    public string GetTranslation(string text)
+    {
+        string translation = "NULL";
+
+        //search for word
+        for (int i = 0; i < contents.Length; i++)
+        {
+            if (contents[i] == text)
+            {
+                translation = contents[i + langIndex];
+            }
+        }
+
+        return translation;
+    }
 }
---
+++ Tools2/Assets/Script/TextAddOn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAddOn : MonoBehaviour {
+
+    public GameObject libraryTester;
+    public LibraryController library;
+    public string englishText;
+    public string translatedText;
+    public Text textUsed;
+
+	// Use this for initialization
+	void Start () {
+        library = libraryTester.GetComponent<LibraryController>();
+        textUsed = gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        translatedText = library.GetTranslation(englishText);
+        textUsed.text = translatedText;
+	}
+}
