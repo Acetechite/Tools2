030b8509695533e0fe4abe7c555508b20ec83e68
Acetechite <31084818+Acetechite@users.noreply.github.com>
2017-09-20 16:02:40 -0400

    Scaffolding for TExt Edit
    adding scaffolding for easier playing with unity text for testing

---
+++ Tools2/Assets/Script/LibraryController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class LibraryController : MonoBehaviour {
+
+    public GameObject menuItem;
+    public Text textHolder;
+    public TextAsset library;
+    public string fileText;
+    public string menuText;
+
+	// Use this for initialization
+	void OnEnable () {
+        fileText = library.text;
+        textHolder = menuItem.GetComponent<Text>();
+        textHolder.text = fileText;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
